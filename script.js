c51fa2578d8efc004d568df01784643d1c992c58
doof <itrololo1703@gmail.com>
2025-09-19 21:09:10 +0000

    Rework game logic, refactor code, add comments

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 "use strict";
 
-/*
-getComputerChoice() randomly returns 0, 1, or 2
-
-getHumanChoice() gets 0-2 via prompt()
-
-^ they both return int displaying the choice
-
-0: rock
-1: paper
-2: scissors
-
-playRound() compares choice, prints win/lose/draw message, returns 0, 1 or 2
-
-0: draw
-1: computer wins
-2: human wins
-
-translateChoice() translates choice int to string
-
-playGame() keeps track of scores, calls playRound 5 times, declares winner
-
-humanScore and computerScore init with 0, increment after win
-*/
-
-
 function getComputerChoice() {
   // 0, 1 or 2
   const choice = Math.floor(Math.random() * 3); 
 
   switch (choice) {
     case 0:
-      return "rock";
+      return 'rock';
     case 1:
-      return "paper";
+      return 'paper';
     case 2:
-      return "scissors";
+      return 'scissors';
+  }
+}
+
+function toggleWinMessage(result) {
+  if (result) {
+    // change the winner and toggle result div
+  }
+}
+
+function checkIfWon() {
+  if (computerScore === 5 || humanScore === 5) {
+    computerScore = 0;
+    humanScore = 0;
+
+    toggleWinMessage(computerScore > humanScore);
+    won = true;
+  }
+}
+
+function decideWinner(humanChoice, computerChoice) {
+  if (computerChoice === humanChoice) {
+    return;
+  } else if (computerChoice === 'rock') {
+    if (humanChoice === 'paper') {
+      humanScore++;
+      return;
+    } else { // scissors
+      computerScore++;
+      return;
+    }
+  } else if (computerChoice === 'paper') {
+    if (humanChoice === 'scissors') {
+      humanScore++;
+      return;
+    } else { // rock
+      computerScore++;
+      return;
+    }
+  } else if (computerChoice === 'scissors') {
+    if (humanChoice === 'rock') {
+      humanScore++;
+      return;
+    } else { // paper
+      computerScore++;
+      return;
+    }
   }
 }
 
@@ -43,6 +64,29 @@ function playRound(element) {
   const humanChoice = element.target.id,
         computerChoice = getComputerChoice();
 
+  decideWinner(humanChoice, computerChoice);
+
+  checkIfWon();
+
+  // disable the win message on the next button press after the actual win
+  if (won) toggleWinMessage();
+}
+
+let humanScore = 0, 
+    computerScore = 0;
+    won = false;
+
+const buttons = document.getElementById('buttons');
+
+buttons.addEventListener('click', (e) => playRound(e))
+
+
+// new TODO: complete toggleWinMessage()
+// add results div and the winner span
+// add scores div and functions to change it
+// add needed js comments
+// make ASCII art for displaying the user and computer choice
+// somehow highlight the win (effect on the score increment, or overlay animation idk)
 
 // TODO: make it change the global score variable;
 // synchronize score variable with div somehow 
@@ -52,78 +96,3 @@ function playRound(element) {
 // probably remove the game thing at all, somehow check everything inline (idk???);
 // do it all without inline loops (idk, check if score===5 on its increment?)
 // clean this shit up and maybe refactor so it's not that ugly;
-
-  if (computerChoice === humanChoice) {
-    console.log(`Tie! You both chose ${translateChoice(computerChoice)}`);
-    return 0; // draw
-  } else if (computerChoice === 0) { // rock
-    if (humanChoice === 1) { // paper
-      console.log("You win! Paper beats rock")
-      return 2; // human wins
-    } else { // scissors
-      console.log("You lose. Rock beats scissors")
-      return 1; // computer wins
-    }
-  } else if (computerChoice === 1) { // paper
-    if (humanChoice === 2) { // scissors
-      console.log("You win! Scissors beat paper")
-      return 2; // human wins
-    } else { // rock
-      console.log("You lose. Paper beats rock")
-      return 1; // computer wins
-    }
-  } else if (computerChoice === 2) { // scissors
-    if (humanChoice === 0) { // rock
-      console.log("You win! Rock beats scissors")
-      return 2; // human wins
-    } else { // paper
-      console.log("You lose. Scissors beat paper")
-      return 1; // computer wins
-    }
-  }
-}
-
-function playGame() {
-  let computerScore = 0;
-  let humanScore = 0;
-  let result;
-
-  while (computerScore < 3 && humanScore < 3) {
-    result = playRound(getComputerChoice(), getHumanChoice());
-    switch (result) {
-      case 0: // tie
-      break;
-      case 1: // computer wins
-      computerScore++;
-      break;
-      case 2: // human wins
-      humanScore++;
-      break
-    }
-    
-    console.log(`The scores are:\n Computer: ${computerScore}\n Human: ${humanScore}`);
-  }
-
-  // console.log((computerScore > humanScore) ? "You lost the game. Try again next time" : "You won the game! Good job");
-}
-
-const buttons = document.getElementById("buttons");
-
-buttons.addEventListener("click", (e) => playRound(e))
-
-
-
-// while (true) { // every loop is a new game
-  
-  // reset DOM scores and stuff
-
-  // while (true) {
-  //   var computerScore = 0,
-  //       humanScore = 0,
-  //       result;
-
-  //   while (computerScore < 5 && humanScore < 5) {
-
-  //   }
-  // }
-// }
